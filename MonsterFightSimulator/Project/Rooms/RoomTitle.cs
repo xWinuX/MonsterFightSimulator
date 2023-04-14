f628fa48c029c284c56cd0f226c9325011252c72
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-04-14 12:08:54 +0200

    removed ridiculous amount of rain particles

--- MonsterFightSimulator/Project/Rooms/RoomTitle.cs
+++ MonsterFightSimulator/Project/Rooms/RoomTitle.cs
@@ -13,7 +13,7 @@ namespace MonsterFightSimulator.Project.Rooms
             AddSpriteObject(1, Game.Camera.Size - SpriteDatabase.SprControls.Data.Size - Vector2Int.One, SpriteDatabase.SprControls);
 
             // Create rain particles
-            for (int i = 0; i < 2000000; i++) { AddGameObject<ActorRainParticle>(0, Vector2Int.Zero); }
+            for (int i = 0; i < 100; i++) { AddGameObject<ActorRainParticle>(0, Vector2Int.Zero); }
         }
     }
 }
